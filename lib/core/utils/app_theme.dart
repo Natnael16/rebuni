494312a9a659aae267dccdc73f73f4e4414ac27c
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2023-06-02 00:35:16 +0300

    Initial commit

---
+++ lib/core/utils/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+//! change to color scheme
+final appTheme = ThemeData(
+  primaryColor: primaryColor,
+  secondaryHeaderColor: secondaryColor,
+  cardColor: containerBackgroundColor,
+  scaffoldBackgroundColor: white,
+);
---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFFFFC069);
+const secondaryColor = Color(0xFFA45D5D);
+const white = Color(0xFFFFFFFF);
+const black = Color(0xFF000000);
+const textFieldColor = Color(0xFFF1F3FC);
+const containerBackgroundColor = Color(0xFFF9F9F9);
+const answeredGreen = Color(0xFF24CE85);
